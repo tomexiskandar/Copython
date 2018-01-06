a8b55435b9baa2f4f786914a7797dfdd69f49af7
Tomex Iskandar <tomex.iskandar@gmail.com>
2018-01-06 22:50:20 +1000

    update test

--- test/_test3_load_oracle_query_into_mssql.py
+++ test/_test3_load_oracle_query_into_mssql.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
     conn_str = "DRIVER={ODBC Driver 11 for SQL Server};SERVER=LATITUDE;PORT=1443;DATABASE=Test;UID=user_name;PWD=password;"
     copython.drop_table(conn_str,"dbo","countries")
     
-    config_path = r"E:\Documents\Visual Studio 2017\Projects\copython\test\_cf_load_oracle_table_into_mssql.xml"
+    config_path = r"E:\Documents\Visual Studio 2017\Projects\copython\test\_cf_load_oracle_query_into_mssql.xml"
     # call copython.copy_data
     res = copython.copy_data(config_path,debug=True)
     print("res={}".format(res))
